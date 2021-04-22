e5127ce307c623fa662c2a3cd743c2d66c038ace
HenryToti <henryokoth3@gmail.com>
2021-04-22 10:31:08 +0300

    1.implementation of the rxjs in react.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect ,useState} from 'react';
+import {from,BehaviorSubject} from 'rxjs';
+import {filter,debounceTime,mergeMap,distinctUntilChanged} from 'rxjs/operators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const searchTodos = async term =>{
+  const todosResponse= await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1000")
+  const todos = await todosResponse.json();          
+ 
+      console.log('term',todos.results);    
+      const searchresult = todos.results.filter(todo => todo.name.includes(term));
+      console.log(searchresult);
+      return  searchresult;
+} 
+
+let searchSubject = new BehaviorSubject('');
+let searchResultObservable = searchSubject.pipe(
+  filter(val => val.length >1),
+  debounceTime(750),
+  distinctUntilChanged(),
+  mergeMap(val => from(searchTodos(val)))
+)
+
+
+const useObservable = (observable ,setter)=>{
+    useEffect(() => {
+      let subscription = observable.subscribe(result=>{
+        setter(result);
+      })
+      return () => {
+        subscription.unsubscribe();
+      }
+    }, [observable,setter]);
+}
+
+function App () {
+
+  const [search, setSearch] = useState('')
+  const [results ,setResults] = useState([]);
+
+  const handleChange = (e)=>{
+    const newValue = e.target.value;
+    setSearch(newValue.toLowerCase());   
+    searchSubject.next(newValue.toLowerCase());
+  }
+
+  useObservable(searchResultObservable,setResults);
+
+  return(
+
+    <div className = "App">
+      <input
+       type = "text"
+       placeholder = "search"
+       value ={search}
+       onChange = {handleChange}      
+      />
+
+    <div>{results.map(pokemon=>(
+      <div key= {pokemon.name}>{pokemon.name}</div>
+    ))}</div>
     </div>
   );
+
 }
 
 export default App;
